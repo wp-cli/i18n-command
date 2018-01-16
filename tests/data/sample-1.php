aeab728d9f78a6242ec4a44a816238638db74a31
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-01-17 01:03:45 +0900

    add phpunit

---
+++ tests/data/sample-1.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Foo Plugin
+ * Plugin URI:  https://example.com
+ * Description:
+ * Version:     0.1.0
+ * Author:
+ * Author URI:
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: foo-plugin
+ * Domain Path: /languages
+ */
+
+__( 'Hello World 1', 'foo-plugin' );
+
+__( 'Foo 1', 'bar' );
+
+__( 'bar 1' );

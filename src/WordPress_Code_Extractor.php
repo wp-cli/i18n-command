24adc32670387b2a03bdc60909f2d7aad9838bf8
Pascal Birchler <hello@pascalbirchler.ch>
2018-01-22 17:25:32 +0100

    Align array items

--- src/WordPress_Code_Extractor.php
+++ src/WordPress_Code_Extractor.php
@@ -16,29 +16,29 @@ class WordPress_Code_Extractor extends PhpCode {
 		'extractComments' => 'translators',
 		'constants'       => [],
 		'functions'       => [
-			'__'                   => 'text_domain',
-			'esc_attr__'           => 'text_domain',
-			'esc_html__'           => 'text_domain',
-			'_e'                   => 'text_domain',
-			'esc_attr_e'           => 'text_domain',
-			'esc_html_e'           => 'text_domain',
-			'_x'                   => 'text_context_domain',
-			'_ex'                  => 'text_context_domain',
-			'esc_attr_x'           => 'text_context_domain',
-			'esc_html_x'           => 'text_context_domain',
-			'_n'                   => 'single_plural_number_domain',
-			'_nx'                  => 'single_plural_number_context_domain',
-			'_n_noop'              => 'single_plural_domain',
-			'_nx_noop'             => 'single_plural_context_domain',
+			'__'              => 'text_domain',
+			'esc_attr__'      => 'text_domain',
+			'esc_html__'      => 'text_domain',
+			'_e'              => 'text_domain',
+			'esc_attr_e'      => 'text_domain',
+			'esc_html_e'      => 'text_domain',
+			'_x'              => 'text_context_domain',
+			'_ex'             => 'text_context_domain',
+			'esc_attr_x'      => 'text_context_domain',
+			'esc_html_x'      => 'text_context_domain',
+			'_n'              => 'single_plural_number_domain',
+			'_nx'             => 'single_plural_number_context_domain',
+			'_n_noop'         => 'single_plural_domain',
+			'_nx_noop'        => 'single_plural_context_domain',
 
 			// Compat.
-			'_'                    => 'gettext', // Same as 'text_domain'.
+			'_'               => 'gettext', // Same as 'text_domain'.
 
 			// Deprecated.
-			'_c'                   => 'text_domain',
-			'_nc'                  => 'single_plural_number_domain',
-			'__ngettext'           => 'single_plural_number_domain',
-			'__ngettext_noop'      => 'single_plural_domain',
+			'_c'              => 'text_domain',
+			'_nc'             => 'single_plural_number_domain',
+			'__ngettext'      => 'single_plural_number_domain',
+			'__ngettext_noop' => 'single_plural_domain',
 		],
 	];
 

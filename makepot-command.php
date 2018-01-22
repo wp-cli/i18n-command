6a7358393102cbf25656fdb4ce38d3fcfc101b1d
Pascal Birchler <hello@pascalbirchler.ch>
2018-01-22 12:32:04 +0100

    Require PHP 5.4 for the makepot command
    See #14.

--- makepot-command.php
+++ makepot-command.php
@@ -10,5 +10,11 @@ if ( file_exists( $autoload ) ) {
 	require_once $autoload;
 }
 
-WP_CLI::add_command( 'makepot', '\WP_CLI\Makepot\Makepot_Command' );
-
+WP_CLI::add_command( 'makepot', '\WP_CLI\Makepot\Makepot_Command', array(
+	'before_invoke' => function() {
+		$min_version = '5.4';
+		if ( version_compare( PHP_VERSION, $min_version, '<' ) ) {
+			WP_CLI::error( "The `wp makepot` command requires PHP {$min_version} or newer." );
+		}
+	}
+) );

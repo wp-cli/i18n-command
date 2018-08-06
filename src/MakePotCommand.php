abf34a59c071ff23d08edc45e6b30228ae582bdf
Pascal Birchler <hello@pascalbirchler.ch>
2018-08-06 22:55:44 +0200

    Incorporate feedback from code review

--- src/MakePotCommand.php
+++ src/MakePotCommand.php
@@ -71,12 +71,12 @@ class MakePotCommand extends WP_CLI_Command {
 	/**
 	 * @var string
 	 */
-	protected $copyright_holder;
+	protected $package_name;
 
 	/**
 	 * @var string
 	 */
-	protected $package_name;
+	protected $file_comment;
 
 	/**
 	 * These Regexes copied from http://php.net/manual/en/function.sprintf.php#93552
@@ -125,9 +125,10 @@ class MakePotCommand extends WP_CLI_Command {
 	)/x';
 
 	/**
-	 * Create a POT file for a WordPress plugin or theme.
+	 * Create a POT file for a WordPress project.
 	 *
-	 * Scans PHP and JavaScript files, as well as theme stylesheets for translatable strings.
+	 * Scans PHP and JavaScript files for translatable strings, as well as theme stylesheets and plugin files
+	 * if the source directory is detected as either a plugin or theme.
 	 *
 	 * ## OPTIONS
 	 *
@@ -143,26 +144,34 @@ class MakePotCommand extends WP_CLI_Command {
 	 * [--domain=<domain>]
 	 * : Text domain to look for in the source code, unless the `--ignore-domain` option is used.
 	 * By default, the "Text Domain" header of the plugin or theme is used.
-	 * If none is provided, it falls back to the plugin/theme slug.
+	 * If none is provided, it falls back to the project slug.
 	 *
 	 * [--ignore-domain]
 	 * : Ignore the text domain completely and extract strings with any text domain.
 	 *
-	 * [--merge[=<files>]]
-	 * : One or more existing POT files whose contents should be merged with the extracted strings.
-	 * If left empty, defaults to the destination POT file.
+	 * [--merge[=<paths>]]
+	 * : Comma-separated list of POT files whose contents should be merged with the extracted strings.
+	 * If left empty, defaults to the destination POT file. POT file headers will be ignored.
 	 *
-	 * [--except=<files>]
-	 * : If set, only strings not already existing in one of the passed POT files will be extracted.
+	 * [--subtract=<paths>]
+	 * : Comma-separated list of POT files whose contents should act as some sort of blacklist for string extraction.
+	 * Any string which is found on that blacklist will not be extracted.
+	 * This can be useful when you want to create multiple POT files from the same source directory with slightly
+	 * different content and no duplicate strings between them.
 	 *
 	 * [--include=<paths>]
-	 * : Only take specific files and folders into account for the string extraction.
+	 * : Comma-separated list of files and paths that should be used for string extraction.
+	 * If provided, only these files and folders will be taken into account for string extraction.
+	 * For example, `--include="src,my-file.php` will ignore anything besides `my-file.php` and files in the `src` directory.
+	 * Simple glob patterns can be used, i.e. `--include=foo-*.php` includes any PHP file with the `foo-` prefix.
 	 * Leading and trailing slashes are ignored, i.e. `/my/directory/` is the same as `my/directory`.
 	 *
 	 * [--exclude=<paths>]
-	 * : Include additional ignored paths as CSV (e.g. 'tests,bin,.github').
-	 * By default, the following files and folders are ignored: node_modules, .git, .svn, .CVS, .hg, vendor.
+	 * : Comma-separated list of files and paths that should be skipped for string extraction.
+	 * For example, `--exclude=".github,myfile.php` would ignore any strings found within `myfile.php` or the `.github` folder.
+	 * Simple glob patterns can be used, i.e. `--exclude=foo-*.php` excludes any PHP file with the `foo-` prefix.
 	 * Leading and trailing slashes are ignored, i.e. `/my/directory/` is the same as `my/directory`.
+	 * The following files and folders are always excluded: node_modules, .git, .svn, .CVS, .hg, vendor, *.min.js.
 	 *
 	 * [--headers=<headers>]
 	 * : Array in JSON format of custom headers which will be added to the POT file. Defaults to empty array.
@@ -170,16 +179,33 @@ class MakePotCommand extends WP_CLI_Command {
 	 * [--skip-js]
 	 * : Skips JavaScript string extraction. Useful when this is done in another build step, e.g. through Babel.
 	 *
-	 * [--copyright-holder=<name>]
-	 * : Name to use for the copyright comment in the resulting POT file.
+	 * [--file-comment]
+	 * : String that should be added as a comment to the top of the resulting POT file.
+	 * By default, a copyright comment is added for WordPress plugins and themes in the following manner:
+	 *
+	 * 		```
+	 * 		Copyright (C) 2018 Example Plugin Author
+	 * 		This file is distributed under the same license as the Example Plugin package.
+	 * 		```
+	 *
+	 * 		If a plugin or theme specifies a license in their main plugin file or stylesheet, the comment looks like this:
+	 *
+	 * 		```
+	 * 		Copyright (C) 2018 Example Plugin Author
+	 * 		This file is distributed under the GPLv2.
+	 * 		```
 	 *
 	 * [--package-name=<name>]
-	 * : Name to use for package name in the resulting POT file. Overrides anything found in a plugin or theme.
+	 * : Name to use for package name in the resulting POT file's `Project-Id-Version` header.
+	 * Overrides plugin or theme name, if applicable.
 	 *
 	 * ## EXAMPLES
 	 *
 	 *     # Create a POT file for the WordPress plugin/theme in the current directory
 	 *     $ wp i18n make-pot . languages/my-plugin.pot
+	 *
+	 *     # Create a POT file for the continents and cities list in WordPress core.
+	 *     $ wp i18n make-pot . continents-and-cities.pot --include="wp-admin/includes/continents-cities.php" --ignore-domain
 	 *
 	 * @when before_wp_load
 	 *
@@ -221,12 +247,12 @@ class MakePotCommand extends WP_CLI_Command {
 		$array_arguments = array( 'headers' );
 		$assoc_args      = Utils\parse_shell_arrays( $assoc_args, $array_arguments );
 
-		$this->source           = realpath( $args[0] );
-		$this->slug             = Utils\get_flag_value( $assoc_args, 'slug', Utils\basename( $this->source ) );
-		$this->skip_js          = Utils\get_flag_value( $assoc_args, 'skip-js', $this->skip_js );
-		$this->headers          = Utils\get_flag_value( $assoc_args, 'headers', $this->headers );
-		$this->package_name     = Utils\get_flag_value( $assoc_args, 'package-name', 'Unknown' );
-		$this->copyright_holder = Utils\get_flag_value( $assoc_args, 'copyright-holder', 'Unknown' );
+		$this->source       = realpath( $args[0] );
+		$this->slug         = Utils\get_flag_value( $assoc_args, 'slug', Utils\basename( $this->source ) );
+		$this->skip_js      = Utils\get_flag_value( $assoc_args, 'skip-js', $this->skip_js );
+		$this->headers      = Utils\get_flag_value( $assoc_args, 'headers', $this->headers );
+		$this->file_comment = Utils\get_flag_value( $assoc_args, 'file-comment' );
+		$this->package_name = Utils\get_flag_value( $assoc_args, 'package-name' );
 
 		$ignore_domain = Utils\get_flag_value( $assoc_args, 'ignore-domain', false );
 
@@ -313,8 +339,8 @@ class MakePotCommand extends WP_CLI_Command {
 
 		$this->exceptions = new Translations();
 
-		if ( isset( $assoc_args['except'] ) ) {
-			$exceptions = explode( ',', $assoc_args['except'] );
+		if ( isset( $assoc_args['subtract'] ) ) {
+			$exceptions = explode( ',', $assoc_args['subtract'] );
 
 			$exceptions = array_filter(
 				$exceptions,
@@ -367,8 +393,6 @@ class MakePotCommand extends WP_CLI_Command {
 
 	/**
 	 * Retrieves the main file data of the plugin or theme.
-	 *
-	 * @throws WP_CLI\ExitException
 	 *
 	 * @return array
 	 */
@@ -634,38 +658,50 @@ class MakePotCommand extends WP_CLI_Command {
 	/**
 	 * Returns the copyright comment for the given package.
 	 *
-	 * @return array Meta data.
+	 * @return string File comment.
 	 */
 	protected function get_file_comment() {
-		$author = $this->copyright_holder;
-		$name   = $this->package_name;
-
-		if ( isset( $this->main_file_data['Theme Name'] ) ) {
-			$name   = $this->main_file_data['Theme Name'];
-			$author = $this->main_file_data['Author'];
-		} elseif ( isset( $this->main_file_data['Plugin Name'] ) ) {
-			$name   = $this->main_file_data['Plugin Name'];
-			$author = $name;
+		if ( $this->file_comment ) {
+			return implode( "\n", explode( '\n', $this->file_comment ) );
 		}
 
-		$author = null === $author ? $this->copyright_holder : $author;
-		$name   = null === $name ? $this->package_name : $name;
+		if ( isset( $this->main_file_data['Theme Name'] ) ) {
+			if ( isset( $this->main_file_data['License'] ) ) {
+				return sprintf(
+					"Copyright (C) %1\$s %2\$s\nThis file is distributed under the %3\$s.",
+					date( 'Y' ),
+					$this->main_file_data['Author'],
+					$this->main_file_data['License']
+				);
+			}
 
-		if ( isset( $this->main_file_data['License'] ) ) {
 			return sprintf(
-				"Copyright (C) %1\$s %2\$s\nThis file is distributed under the %3\$s.",
+				"Copyright (C) %1\$s %2\$s\nThis file is distributed under the same license as the %3\$s theme.",
 				date( 'Y' ),
-				$author,
-				$this->main_file_data['License']
+				$this->main_file_data['Author'],
+				$this->main_file_data['Theme Name']
 			);
 		}
 
-		return sprintf(
-			"Copyright (C) %1\$s %2\$s\nThis file is distributed under the same license as the %3\$s package.",
-			date( 'Y' ),
-			$author,
-			$name
-		);
+		if ( isset( $this->main_file_data['Plugin Name'] ) ) {
+			if ( isset( $this->main_file_data['License'] ) ) {
+				return sprintf(
+					"Copyright (C) %1\$s %2\$s\nThis file is distributed under the %3\$s.",
+					date( 'Y' ),
+					$this->main_file_data['Author'],
+					$this->main_file_data['License']
+				);
+			}
+
+			return sprintf(
+				"Copyright (C) %1\$s %2\$s\nThis file is distributed under the same license as the %3\$s plugin.",
+				date( 'Y' ),
+				$this->main_file_data['Author'],
+				$this->main_file_data['Plugin Name']
+			);
+		}
+
+		return '';
 	}
 
 	/**
@@ -674,7 +710,7 @@ class MakePotCommand extends WP_CLI_Command {
 	 * @param Translations $translations Translations object.
 	 */
 	protected function set_default_headers( $translations ) {
-		$name         = $this->package_name;
+		$name         = null;
 		$version      = $this->get_wp_version();
 		$bugs_address = null;
 
@@ -690,9 +726,13 @@ class MakePotCommand extends WP_CLI_Command {
 			$bugs_address = sprintf( 'https://wordpress.org/support/plugin/%s', $this->slug );
 		}
 
-		$name = null === $name ? $this->package_name : $name;
+		if ( null !== $this->package_name ) {
+			$name = $this->package_name;
+		}
 
-		$translations->setHeader( 'Project-Id-Version', $name . ( $version ? ' ' . $version : '' ) );
+		if ( null !== $name ) {
+			$translations->setHeader( 'Project-Id-Version', $name . ( $version ? ' ' . $version : '' ) );
+		}
 
 		if ( null !== $bugs_address ) {
 			$translations->setHeader( 'Report-Msgid-Bugs-To', $bugs_address );
